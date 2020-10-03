64b3446f3e935400390148de752cd0d7fc5b609a
Kelly Danger <kellyanndanger@gmail.com>
2020-10-03 15:05:39 -0500

    inputs and button working

--- scripts.js
+++ scripts.js
@@ -1,6 +1,31 @@
-console.log("JS");
+
 $(document).ready(onReady);
 
 function onReady(){
-  console.log('JQ');
-}
+  $(employeeInfoButton).on('click', buttonFunc);
+}//end onReady
+
+function buttonFunc(){
+  listEmpInfo();
+  calcMonthlyCosts();
+  $('.input').val('');
+  console.log('I was clicked')
+}//end buttonFunc
+
+function calcMonthlyCosts(){
+  console.log('whoah, that is expensive')
+}//end calculateMonthlyCosts
+
+
+function listEmpInfo(){
+  let newEmployee = {
+    firstName: $('#employeeFirstInput').val(),
+    lastName: $('#employeeLastInput').val(),
+    idNumber: $('#idNumberInput').val(),
+    title: $('#jobTitleInput').val(),
+    salary: $('#salarayInput')
+  }
+  console.log(newEmployee);
+}//end storeEmpInfo
+
+
